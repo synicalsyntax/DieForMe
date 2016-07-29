b553304b1df700630490cc8cf142aaf8ade17bbd
Cynthia Lin <icylightning282@gmail.com>
2016-07-28 22:48:36 -0700

    MovingPlatform code beautification

--- Assets/Scripts/MovingPlatform.js
+++ Assets/Scripts/MovingPlatform.js
@@ -1,21 +1,19 @@
 ﻿#pragma strict
-public var speed : float=2;
-public var wait : float =1;
-public var startingX: float =-5;
-public var endingX: float =5;
-function Start () {
-    
-}
 
-function Update () {
+public var speed: float = 3;
+public var wait: float = 0.5;
+public var startingX: float = -5;
+public var endingX: float = 5;
+
+function Update() {
     Move();
 }
-function PingPong(t : float,minLength: float,maxLength: float){
-    var pos: float = (Mathf.PingPong(t,maxLength-minLength)+minLength);
-    
-        return pos;
-}
-function Move(){
-    transform.position= new Vector3(PingPong(Time.time*speed,startingX,endingX),transform.position.y,transform.position.z);
 
+function PingPong(t: float, minLength: float, maxLength: float) {
+    var pos: float = (Mathf.PingPong(t, maxLength - minLength) + minLength);
+    return pos;
+}
+
+function Move() {
+    transform.position = new Vector3(PingPong(Time.time * speed, startingX, endingX), transform.position.y, transform.position.z);
 }
