b7f4b9716b477895ea42fe49b96e2722dd67eb0b
Cynthia Lin <icylightning282@gmail.com>
2016-07-28 23:00:58 -0700

    Commenting & beautification

--- Assets/Scripts/EnemyRotation.js
+++ Assets/Scripts/EnemyRotation.js
@@ -1,11 +1,18 @@
-﻿#pragma strict
+﻿/*
+Enemy Rotation
+  Enemy is set under Game Object parent of EnemyRotation at origin
+  Enemy is offset to desired position
+*/
 
-var rotation : Quaternion;
-var radius = Vector3(5,0,0);
-var currentRotation = 0.0;
+#pragma strict
+
+var rotation : Quaternion; //used to denote rotations
+public var radius : Vector3; //set radius of 1 in EnemyRotation
+var currentRotation = 0.0; //set default rotation to 0
+
 function Update()
 {
-    currentRotation += Time.deltaTime*100;
-    rotation.eulerAngles = Vector3(0, 0, currentRotation);
-    transform.position = rotation * radius;
+    currentRotation += Time.deltaTime * 100; //sets currentRotation to amount of time passed
+    rotation.eulerAngles = Vector3(0, 0, currentRotation); //sets rotation around z axis to currentRotation (represented as Euler angles in degrees)
+    transform.position = rotation * radius; //translates Enemy position according to set radius
 }
--- Assets/Scripts/MovingPlatform.js
+++ Assets/Scripts/MovingPlatform.js
@@ -1,19 +1,15 @@
 ﻿#pragma strict
 
-public var speed: float = 3;
-public var wait: float = 0.5;
-public var startingX: float = -5;
-public var endingX: float = 5;
+public var speed: float;
+public var wait: float;
+public var startingX: float;
+public var endingX: float;
 
 function Update() {
-    Move();
+    transform.position = new Vector3(PingPong(Time.time * speed, startingX, endingX), transform.position.y, transform.position.z);
 }
 
 function PingPong(t: float, minLength: float, maxLength: float) {
     var pos: float = (Mathf.PingPong(t, maxLength - minLength) + minLength);
     return pos;
-}
-
-function Move() {
-    transform.position = new Vector3(PingPong(Time.time * speed, startingX, endingX), transform.position.y, transform.position.z);
 }
