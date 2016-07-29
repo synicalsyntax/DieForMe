7f0be7a9b5fcfd01500b7af1045ee7966d7e4faf
Cynthia Lin <icylightning282@gmail.com>
2016-07-28 22:40:06 -0700

    Rotating enemy

---
+++ Assets/Scripts/EnemyRotation.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var rotation : Quaternion;
+var radius = Vector3(5,0,0);
+var currentRotation = 0.0;
+function Update()
+{
+    currentRotation += Time.deltaTime*100;
+    rotation.eulerAngles = Vector3(0, 0, currentRotation);
+    transform.position = rotation * radius;
+}
