1f4153db8722fdbaf19ef822ad1949f3ce380748
Cynthia Lin <icylightning282@gmail.com>
2016-07-30 11:56:44 -0700

    Lunchtime yay

--- Assets/Scripts/PlayerControl.js
+++ Assets/Scripts/PlayerControl.js
@@ -8,7 +8,6 @@ Basic Player Platformer Script:
 #pragma strict
 
 var isgrounded: boolean = true; //variable for when player is grounded
-//public var projectile : GameObject;
 public var SpellA : GameObject;
 public var SpellS : GameObject;
 
@@ -19,22 +18,17 @@ function Start() {
 function FixedUpdate() {
     var moveHorizontal: float = Input.GetAxis("Horizontal");
     var horizontalForce: Vector3 = new Vector3(moveHorizontal, 0, 0);
-    GetComponent.<Rigidbody>().AddForce(horizontalForce * 70); //20 value can be changed according to player speed
+    GetComponent.<Rigidbody>().AddForce(horizontalForce * 25); //20 value can be changed according to player speed
 
     if (isgrounded == true) {
         if (Input.GetKeyDown("up") || Input.GetKeyDown("w")) {
-            GetComponent.<Rigidbody>().AddForce(Vector3(0, 1000, 0)); //might depend on mass of object
+            GetComponent.<Rigidbody>().AddForce(Vector3(0, 750, 0)); //might depend on mass of object
         }
     }
 
     if (transform.position.y < -10) { //if player falls off a platform or something
         transform.position = Vector3(0, 2, 0); //return to original position
         transform.Rotate(0, 0, 0); //resets rotation, idk if player will be rotating
-    }
-
-    if (Input.GetKeyDown(KeyCode.Space)) {
-        var newProjectile = Instantiate(projectile, transform.position, transform.rotation);
-        newProjectile.transform.position = Vector3.MoveTowards(transform.position, Vector3(transform.position.x + 10, transform.position.y, transform.position.z), 0);
     }
 }
 
@@ -51,10 +45,11 @@ function OnCollisionEnter(theCollision : Collision) {
 
 function OnCollisionStay(theCollision : Collision) {
     if (theCollision.gameObject.name.StartsWith("Platform Moving")) { //while colliding with object name that starts with Platform Moving
-        transform.position = Vector3(GameObject.Find("Platform Moving").transform.position.x, transform.position.y, transform.position.z); //translate position to x position of Moving platform
+        transform.parent = GameObject.Find("Platform Moving").transform; //make Player child of Moving Platformer so its position will be offset accordingly
     }
 }
 
 function OnCollisionExit(theCollision : Collision) {
     isgrounded = false; //sets isgrounded to false once not colliding with an object
+    transform.parent = null;
 }
