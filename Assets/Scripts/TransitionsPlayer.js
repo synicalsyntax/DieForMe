b506dbfd5b7451fb750b5dea99f9056edbde60ef
cicizhang01 <cicizhang01@yahoo.com>
2016-08-01 14:03:05 -0700

    IT RUNS

--- Assets/Scripts/TransitionsPlayer.js
+++ Assets/Scripts/TransitionsPlayer.js
@@ -1,9 +1,24 @@
 ﻿#pragma strict
 
+
+private var animator : Animator;
+
+
+
 function Start () {
+    animator = GetComponent.<Animator>();
+    
+    
 
 }
 
 function Update () {
-
+    var moveHorizontal: float = Input.GetAxis("Horizontal");
+    if(moveHorizontal ==0){
+        animator.SetBool("Running",false);
+    }
+    else{
+        animator.SetBool("Running",true);
+    }
+    
 }
