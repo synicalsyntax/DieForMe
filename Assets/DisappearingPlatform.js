7e66d3bbf1d474271893514f181059ab9c206c25
jessicashu7 <jessicashu7@gmail.com>
2016-07-31 21:25:22 -0700

    disappearing platforms
    i added a script for all the disappearing platforms, but haven't tried
    it out yet

---
+++ Assets/DisappearingPlatform.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+private var playerObject : GameObject;
+
+function Start () {
+    playerObject = GameObject.FindWithTag("Player");
+
+}
+
+function Update () {
+
+}
+
+
+function OnCollisionExit2D(theCollision : Collision2D) {
+    if (theCollision.gameObject.name.StartsWith("Player")){
+        gameObject.SetActive(false);
+
+    }
+}
