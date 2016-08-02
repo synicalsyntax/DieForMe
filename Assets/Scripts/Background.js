73aec6638e9c90dd14290e5ae9850260b9f55e9a
Cynthia Lin <cynthialin1004@gmail.com>
2016-08-02 14:41:44 -0700

    TADA BACKGROUND

---
+++ Assets/Scripts/Background.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+#pragma strict
+
+var target : Transform;
+var distance : float;
+
+function Update(){
+    transform.position = new Vector3(GameObject.Find("Player").transform.position.x, GameObject.Find("Player").transform.position.y, 5);
+}
