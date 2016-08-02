5b7629b7a409168d5476eda846b283df30785baa
jessicashu7 <jessicashu7@gmail.com>
2016-08-01 23:06:46 -0700

    wow i think i fixed it

--- Assets/Scripts/Dementor.js
+++ Assets/Scripts/Dementor.js
@@ -5,6 +5,9 @@ public var speed: float =3;
 public var startingX: float;
 public var endingX: float;
 
+function Start(){
+gameObject.SetActive(true);
+}
 function Update() {
     transform.position = new Vector3(PingPong(Time.time * speed, startingX, endingX), transform.position.y, transform.position.z);
 }
@@ -15,10 +18,17 @@ function PingPong(t: float, minLength: float, maxLength: float) {
 }
 
 
-function OnCollisionEnter2D(theCollision : Collision2D) {
-    if (theCollision.gameObject.name.StartsWith("SpellZ(Clone)")) {
-    Debug.Log("hit");
-    Destroy(theCollision.gameObject);
-    gameObject.SetActive(false);
-      }
+//function OnCollisionEnter2D(theCollision : Collision2D) {
+   // if (theCollision.gameObject.name.StartsWith("SpellZ(Clone)")) {
+   // Debug.Log("hit");
+  //  Destroy(theCollision.gameObject);
+  //  gameObject.SetActive(false);
+  //    }
+//}
+    function OnTriggerEnter2D(collider2D : Collider2D){
+        if (collider2D.name.StartsWith("SpellZ")){
+            Destroy(collider2D.gameObject);
+            gameObject.SetActive(false);
+
+        }
     }
