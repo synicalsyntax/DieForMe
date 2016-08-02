0af325f151a6829b400e427e46c097c6873d24f7
jessicashu7 <jessicashu7@gmail.com>
2016-08-02 10:37:19 -0700

    stuck on how to make auror/dementor rotation

--- Assets/Scripts/AurorRotation.js
+++ Assets/Scripts/AurorRotation.js
@@ -7,9 +7,6 @@ public var AurorSpell : Rigidbody2D;
 public var spellSpeed : int;
 public var direction : float;
 
-var rotation : Quaternion; //used to denote rotations
-public var radius : Vector3; //set radius of 1 in EnemyRotation
-var currentRotation = 0.0; //set default rotation to 0
 
 function Start(){
     GetComponent.<Renderer>().enabled = true;
@@ -22,9 +19,7 @@ function Update() {
         SpellCoolDown = Time.time + duration; 
         Spell();
     }
-    currentRotation += Time.deltaTime * 100; //sets currentRotation to amount of time passed
-    rotation.eulerAngles = Vector3(0, 0, currentRotation); //sets rotation around z axis to currentRotation (represented as Euler angles in degrees)
-    transform.position = rotation * radius; //translates Enemy position according to set radius
+  
 }
 
     function OnTriggerEnter2D(collider2D : Collider2D){
---
+++ Assets/Scripts/DementorRotation.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+
+
+function Start(){
+    GetComponent.<Renderer>().enabled = true;
+    GetComponent.<Collider2D>().enabled = true;
+}
+
+function Update() {
+
+}
+
+
+    function OnTriggerEnter2D(collider2D : Collider2D){
+        if (collider2D.name.StartsWith("SpellZ")){
+            GetComponent.<SpriteRenderer>().enabled = false;
+            GetComponent.<Collider2D>().enabled = false;
+            Destroy(collider2D.gameObject);
+        }
+    }
