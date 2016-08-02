908bbbcf1124cbb3f05217581c6653c59449318b
cicizhang01 <cicizhang01@yahoo.com>
2016-08-02 09:42:27 -0700

    Text Splits

--- Assets/Scripts/Introduction.js
+++ Assets/Scripts/Introduction.js
@@ -1,6 +1,23 @@
 ﻿#pragma strict
+import UnityEngine.UI;
+import UnityEngine.Networking;
+
+
+public var textBox : GameObject;
+public var theText : Text;
+public var textFile : TextAsset;
+public var textLines : String[];
+public var currentLine : float;
+public var endAtLine : float;
+public var player : PlayerController;
+
 
 function Start () {
+    var temp : String = textFile.text;
+    textLines = temp.Split("\n"[0]);
+    //var yay : String = "Hello, hey";
+    
+    //print(yay.Split(","[0]));
 
 }
 
