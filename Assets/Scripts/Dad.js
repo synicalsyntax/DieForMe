5308d1ee5de97e293ae07a5dc8d1e4ee594707b3
cicizhang01 <cicizhang01@yahoo.com>
2016-08-03 09:59:43 -0700

    Dad dies and sluggy moves slower

---
+++ Assets/Scripts/Dad.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+//public var speed: float =3;
+public var startingX: float;
+public var endingX: float;
+
+function Start(){
+    GetComponent.<Renderer>().enabled = true;
+    GetComponent.<Collider2D>().enabled = true;
+}
+function Update() {
+    //transform.position = new Vector3(PingPong(Time.time * speed, startingX, endingX), transform.position.y, transform.position.z);
+}
+
+
+
+    function OnTriggerEnter2D(collider2D : Collider2D){
+        if (collider2D.name.StartsWith("SpellZ")){
+            GetComponent.<SpriteRenderer>().enabled = false;
+            GetComponent.<Collider2D>().enabled = false;
+        }
+        if (collider2D.name.StartsWith("SpellX")){
+            GetComponent.<SpriteRenderer>().enabled = false;
+            GetComponent.<Collider2D>().enabled = false;
+        }
+    }
