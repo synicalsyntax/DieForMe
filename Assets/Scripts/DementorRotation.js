313f9d7631832ac9a83d6459b373956e2af97aff
Cynthia Lin <cynthialin1004@gmail.com>
2016-08-02 21:36:30 -0700

    dementors are back

--- Assets/Scripts/DementorRotation.js
+++ Assets/Scripts/DementorRotation.js
@@ -9,14 +9,6 @@ function Start(){
     GetComponent.<Collider2D>().enabled = true;
 }
 
-function Update() {
-   // transform.position = new Vector3(PingPong(Time.time * speed, startingX, endingX), transform.position.y, transform.position.z);
-}
-
-//function PingPong(t: float, minLength: float, maxLength: float) {
-  //  var pos: float = (Mathf.PingPong(t, maxLength - minLength) + minLength);
- //   return pos;
-//}
     function OnTriggerEnter2D(collider2D : Collider2D){
         if (collider2D.name.StartsWith("SpellZ")){
             GetComponent.<SpriteRenderer>().enabled = false;
