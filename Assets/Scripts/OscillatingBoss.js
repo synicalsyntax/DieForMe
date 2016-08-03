ddffeabdb620e1257d3894b3f52949fc5641daa2
Cynthia Lin <cynthialin1004@gmail.com>
2016-08-03 13:42:05 -0700

    hit

--- Assets/Scripts/OscillatingBoss.js
+++ Assets/Scripts/OscillatingBoss.js
@@ -39,7 +39,7 @@ function PingPong(t: float, minLength: float, maxLength: float) {
 }
 
     function OnTriggerEnter2D(collider2D : Collider2D){
-        if (collider2D.name.StartsWith("SpellX") || collider2D.name.StartsWith("SpellZ")){
+        if (collider2D.gameObject.name.StartsWith('SpellX') || collider2D.name.StartsWith('SpellZ')){
             hits++;
             if (hits > 2) {
                 GetComponent.<SpriteRenderer>().enabled = false;
