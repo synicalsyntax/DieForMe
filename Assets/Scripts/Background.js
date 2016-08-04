21436bf17f5e956e6ddd0f9e28abbde1b1a6518c
innopoop <ashleyrawr74@gmail.com>
2016-08-04 13:14:14 -0700

    fixed background??

--- Assets/Scripts/Background.js
+++ Assets/Scripts/Background.js
@@ -6,5 +6,5 @@ public var offsety : float;
 public var z : float;
 
 function Update(){
-    transform.position = new Vector3(target.transform.position.x + offsetx, target.transform.position.y + offsety, z); //sets Camera position to x and y position of Player (z position value is constant at -10)
+    transform.position = new Vector3(target.transform.position.x + offsetx, target.transform.position.y + offsety, 10); //sets Camera position to x and y position of Player (z position value is constant at -10)
 }
