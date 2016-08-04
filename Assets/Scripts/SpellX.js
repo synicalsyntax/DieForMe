1b2fb6fa39c46244aff0783ecf7f76eb4a0d39a7
jessicashu7 <jessicashu7@gmail.com>
2016-08-04 11:03:26 -0700

    uytrgfe
    tyhgrfedw

--- Assets/Scripts/SpellX.js
+++ Assets/Scripts/SpellX.js
@@ -2,14 +2,26 @@
 
 public var collide : Sprite;
 
+
+public var HitBoss : AudioClip;
+
 function OnBecameInvisible() {
     Destroy(gameObject);
 } 
 
 function OnTriggerEnter2D(collider2D : Collider2D){
-    if (collider2D.name.StartsWith("Platform") || collider2D.name.StartsWith("Auror") || collider2D.name.StartsWith("Lava") || collider2D.name.StartsWith("Big Bad Mafia Boss") || collider2D.name.StartsWith("Big Bad")|| collider2D.name.StartsWith("the dad")){
+    if (collider2D.name.StartsWith("Platform") || collider2D.name.StartsWith("Auror") || collider2D.name.StartsWith("Lava") || collider2D.name.StartsWith("the dad")){
     	GetComponent.<SpriteRenderer>().sprite = collide;
     	yield WaitForSeconds(0.05);
    		Destroy(gameObject); 
+    }
+
+    if (collider2D.gameObject.name.StartsWith("Big Bad")){
+        GetComponent.<AudioSource>().clip = HitBoss;
+        GetComponent.<AudioSource>().Play();
+
+        GetComponent.<SpriteRenderer>().sprite = collide;
+        yield WaitForSeconds(0.05);
+        Destroy(gameObject); 
     }
 }
