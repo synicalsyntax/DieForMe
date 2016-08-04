b660775bb1bfb35135903db583a4acb54fad3bec
Cynthia Lin <cynthialin1004@gmail.com>
2016-08-04 15:28:45 -0700

    redirect to title screen

--- Assets/Credits.js
+++ Assets/Credits.js
@@ -1,12 +1,16 @@
 ﻿#pragma strict
 
+import UnityEngine.SceneManagement;
+
 var speed = 0.2;
 var crawling = false;
-var limit = 425;
+var limit : float;
 
 function Update ()
 {
      if (gameObject.transform.position.y < limit) {
           transform.position.y++;
+     } else {
+     	SceneManager.LoadScene('Title Screen');
      }
 }
