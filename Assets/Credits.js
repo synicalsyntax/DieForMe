85ee199a6e044263ff0ec2f9a265827c20906d75
Cynthia Lin <cynthialin1004@gmail.com>
2016-08-04 15:23:22 -0700

    CREDITS :D

---
+++ Assets/Credits.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var speed = 0.2;
+var crawling = false;
+var limit = 425;
+
+function Update ()
+{
+     if (gameObject.transform.position.y < limit) {
+          transform.position.y++;
+     }
+}
